2a8c8503e01965d767ae18f10ba189767f747ba9
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-19 23:30:11 +0000

    save changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import BestMatch from "./components/BestMatch";
 import TopSlider from "./components/TopSlider";
 import Products from "./components/Products";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
 
           <Route path="/products">
             <Header />
+            {/* <Filter /> */}
             <Products />
           </Route>
 
@@ -25,6 +28,7 @@ function App() {
             <Header />
             <Search />
             <TopSlider />
+            <Footer />
           </Route>
         </Switch>
       </div>
