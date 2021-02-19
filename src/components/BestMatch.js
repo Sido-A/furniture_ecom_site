8dbc525e2affdaa994c9bcf776e0e372329f7e18
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-19 23:27:42 +0000

    add tags, base style

---
+++ src/components/BestMatch.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function BestMatch() {
+  return (
+    <div className="bestMatch">
+      <div className="bestMatch__selectField">
+        <select>
+          <option value="#">Best match</option>
+          <option value="#">Best match</option>
+          <option value="#">Best match</option>
+          <option value="#">Best match</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default BestMatch;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,18 @@
 import React from "react";
+import BestMatch from "./BestMatch";
 import SearchIcon from "@material-ui/icons/Search";
 
 function Search() {
   return (
-    <div className="search container">
-      <SearchIcon />
+    <div className="sb container">
+      <div className="search ">
+        <div className="search__field">
+          <SearchIcon className="searchIcon" />
+          <input type="text" />
+        </div>
+      </div>
+
+      <BestMatch />
     </div>
   );
 }
