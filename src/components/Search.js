ccdc8e73f37ae8ae0e8802935c8af8bf4433075e
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-19 21:05:26 +0000

    adding search icon

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Search() {
+  return (
+    <div className="search container">
+      <SearchIcon />
+    </div>
+  );
+}
+
+export default Search;
