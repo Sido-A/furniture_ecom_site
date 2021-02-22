9a689d9d0e35c09249bd846ee305689968dea61a
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-22 12:29:04 +0000

    adding pulling data from firestore

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "./Product";
 import Page from "./Page";
+import db from "../firebase";
 
 function Products() {
+  const [furnitureData, setFurnitureData] = useState([]);
+
+  useEffect(() => {
+    db.collection("furnitures")
+      .get()
+      .then((snap) => {
+        const data = snap.docs.map((doc) => ({
+          id: doc.uid,
+          ...doc.data(),
+        }));
+        console.log(data);
+        setFurnitureData(data);
+      });
+  }, []);
+
   const pagesArr = [1, 2, 3, 4, ">"];
   return (
     <div className="products">
       <div className="products__container">
-        <Product />
-        <Product />
+        {furnitureData.map((data) => (
+          <Product data={data} />
+        ))}
       </div>
 
       <div className="products__pages">
