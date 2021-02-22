79db300c15f90a18d21ea80416c94f898f9f2461
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-22 12:33:08 +0000

    add firebase firestore and hosting

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZtBt5iJnrfZmFBh5woY35uFIHHWx8FgM",
+  authDomain: "ec-furniture-clone.firebaseapp.com",
+  projectId: "ec-furniture-clone",
+  storageBucket: "ec-furniture-clone.appspot.com",
+  messagingSenderId: "419681919040",
+  appId: "1:419681919040:web:e5b57d13484f591233bfa5",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export default db;
