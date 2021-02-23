177fef8b6182e5da314b46e9aa7803318b424534
Sido-A <sidonie.abe.uk@gmail.com>
2021-02-23 16:49:18 +0000

    update syle sheet

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import db from "./firebase";
+
 import Header from "./components/Header";
 import Search from "./components/Search";
 import ProductsFilterContainer from "./components/ProductsFilterContainer";
-import Filter from "./components/Filters";
-import TopSlider from "./components/TopSlider";
 import Products from "./components/Products";
+import TopSlider from "./components/TopSlider";
 import Footer from "./components/Footer";
 
 function App() {
+  const [furnitureData, setFurnitureData] = useState([]);
+
+  useEffect(() => {
+    db.collection("furnitures")
+      .get()
+      .then((snap) => {
+        const data = snap.docs.map((doc) => doc.data());
+
+        setFurnitureData(data);
+      });
+  }, []);
+
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/mag">
+          <Route path="/magazine">
             <Header />
-            <h1>Mag</h1>
+            <Products />
             <Footer />
           </Route>
 
           <Route path="/products">
             <Header />
             <Search />
-            {/* <Filter />
-            <Products /> */}
             <ProductsFilterContainer />
             <Footer />
           </Route>
