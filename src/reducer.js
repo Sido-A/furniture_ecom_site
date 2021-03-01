02730d66b62fe59a6a9be5ca10a26fcc8275cabf
Sido-A <sidonie.abe.uk@gmail.com>
2021-03-01 16:06:15 +0000

    modified filter logic

--- src/reducer.js
+++ src/reducer.js
@@ -16,23 +16,25 @@ const reducer = (state, action) => {
       };
 
     case "ADD_FILTER":
+      console.log("ADD_FILTER");
       const { checkedItems, products } = action;
-
-      console.log("check 1", checkedItems);
 
       const cloneProducts = [...products];
       let localProducts = [];
 
       const testProducts = cloneProducts
         .map((product) => {
-          //check if collection has been ticked
+          //check if 'collection' has been ticked
           if (checkedItems.collection.length !== 0) {
             for (let i = 0; i < checkedItems.collection.length; i++) {
-              // if collection matches check if any other check list were checked
+              // if 'collection' **matches**
+              // check if 'color' has been ticked
               if (product.name === checkedItems.collection[i]) {
                 if (checkedItems.color.length !== 0) {
                   for (let i = 0; i < product.colors.length; i++) {
                     for (let j = 0; j < checkedItems.color.length; j++) {
+                      // if 'color' **matches**
+                      // check if 'category' has been ticked
                       if (product.colors[i] === checkedItems.color[j]) {
                         if (checkedItems.category.length !== 0) {
                           for (
@@ -41,16 +43,20 @@ const reducer = (state, action) => {
                             i++
                           ) {
                             if (product.type === checkedItems.category[i]) {
-                              // return collection, color, category all matched
+                              // return 'collection', 'color', 'category'
+                              //**all matched**
+
                               return product;
                             }
                           }
                         } else {
-                          // return collection,color matches but category un-matched
+                          // return only 'collection','color' matched
+                          //**category un-matched**
+
                           return product;
                         }
                       } else {
-                        // if no color match check category
+                        // if 'collection' **matched** but 'no color' match, move to ''check category''
                         if (checkedItems.category.length !== 0) {
                           for (
                             let i = 0;
@@ -58,32 +64,45 @@ const reducer = (state, action) => {
                             i++
                           ) {
                             if (product.type === checkedItems.category[i]) {
-                              // return collection and category that matches
+                              // return 'collection' and 'category' that matches
+                              // no color **matched**
+
                               return product;
+                            } else {
+                              // "Zero match"
+                              return;
                             }
                           }
                         } else {
-                          // if collection exist but no color, no category list checked return product
-                          return product;
+                          // "Zero match"
+
+                          return;
                         }
                       }
                     }
                   }
                 } else {
+                  // if 'collection' **matched** but no 'tick on color', move to ''check category''
                   if (checkedItems.category.length !== 0) {
                     for (let i = 0; i < checkedItems.category.length; i++) {
                       if (product.type === checkedItems.category[i]) {
-                        // return collection, color, category all matched
+                        // return collection, category matched
+                        // no color chosen by the user
                         return product;
+                      } else {
+                        // "Zero match"
+                        return;
                       }
                     }
                   } else {
-                    // return collection,color matches but category un-matched
+                    // return 'collection' **matched**
+                    // no tick on color **category un-matched**
+
                     return product;
                   }
                 }
-                // if no other check list aren't checked return collections matched product
-                console.log("return collection only", product);
+                // if no other check list aren't checked
+                // return 'collection' matched product
 
                 return product;
               }
@@ -101,6 +120,7 @@ const reducer = (state, action) => {
                     }
                   } else {
                     // return color matched only if no category checked
+
                     return product;
                   }
                 }
@@ -112,62 +132,14 @@ const reducer = (state, action) => {
                 return product;
               }
             }
+          } else {
+            // "No match"
+            return;
           }
         })
         .filter(Boolean);
 
-      console.log("test products", testProducts);
       localProducts = Object.assign([], testProducts);
-
-      // if (
-      //   checkedItems.hasOwnProperty("collection") &&
-      //   checkedItems.collection.length !== 0
-      // ) {
-      //   const collection = cloneProducts
-      //     .map((product) => {
-      //       for (let i = 0; i < checkedItems.collection.length; i++) {
-      //         if (product.name === checkedItems.collection[i]) {
-      //           return product;
-      //         }
-      //       }
-      //     })
-      //     .filter(Boolean);
-      //   localProducts = Object.assign([], collection);
-      // } else if (
-      //   checkedItems.hasOwnProperty("color") &&
-      //   checkedItems.color.length !== 0
-      // ) {
-      //   //color sort
-      //   const color = cloneProducts
-      //     .map((product) => {
-      //       for (let i = 0; i < product.colors.length; i++) {
-      //         for (let j = 0; j < checkedItems.color.length; j++) {
-      //           if (product.colors[i] === checkedItems.color[j]) {
-      //             return product;
-      //           }
-      //         }
-      //       }
-      //     })
-      //     .filter(Boolean);
-
-      //   localProducts = Object.assign([], color);
-      // } else if (
-      //   checkedItems.hasOwnProperty("category") &&
-      //   checkedItems.category.length !== 0
-      // ) {
-      //   console.log("check 2", checkedItems);
-
-      //   const category = cloneProducts
-      //     .map((product) => {
-      //       for (let i = 0; i < checkedItems.category.length; i++) {
-      //         if (product.type === checkedItems.category[i]) {
-      //           return product;
-      //         }
-      //       }
-      //     })
-      //     .filter(Boolean);
-      //   localProducts = Object.assign([], category);
-      // }
 
       return {
         ...state,
