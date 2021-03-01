3b4b35a00a7eae0b022630aa6b3d7acf5dc9c476
Sido-A <sidonie.abe.uk@gmail.com>
2021-03-01 16:08:04 +0000

    add filtering logic

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
 import RemoveIcon from "@material-ui/icons/Remove";
 import Filter from "./Filter";
+import PriceRange from "./PriceRange";
 import db from "../firebase";
+import { useStateValue } from "../StateProvider";
 
 function Filters() {
   const [sliderValue, setSliderValue] = useState(0);
+  const [sliderMaxValue, setSliderMaxValue] = useState(5000);
   const [mobileFilterMenuToggler, setMobileFilterMenuToggler] = useState(false);
   const [furnitureData, setFurnitureData] = useState([]);
+  const [change, setChange] = useState(false);
+  const [checkedItems, setCheckedItems] = useState({
+    collection: [],
+    color: [],
+    category: [],
+  });
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
     db.collection("furnitures")
@@ -16,10 +26,65 @@ function Filters() {
           id: doc.id,
           ...doc.data(),
         }));
-        console.log(data);
         setFurnitureData(data);
       });
   }, []);
+
+  useEffect(() => {
+    db.collection("furnitures")
+      .get()
+      .then((snap) => {
+        const data = snap.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setFurnitureData(data);
+        filterDispatchHandler(data);
+      })
+      .catch((err) => console.assert(err));
+
+    const filterDispatchHandler = (data) => {
+      if (
+        checkedItems.collection.length === 0 &&
+        checkedItems.color.length === 0 &&
+        checkedItems.category.length === 0
+      ) {
+        dispatch({
+          type: "REMOVE_FILTER",
+          products: data,
+        });
+      } else {
+        dispatch({
+          type: "ADD_FILTER",
+          checkedItems,
+          products: furnitureData,
+        });
+      }
+    };
+  }, [checkedItems]);
+
+  const changeDetector = (e) => {
+    const filterType = e.target.getAttribute("data-filter-type");
+
+    if (e.target.checked === true) {
+      setChange(true);
+
+      setCheckedItems({
+        ...checkedItems,
+        [filterType]: [...checkedItems[filterType], e.target.name],
+      });
+    } else {
+      const unTick = checkedItems[filterType].filter((item) =>
+        item !== e.target.name ? item : null
+      );
+      setChange(false);
+
+      setCheckedItems({
+        ...checkedItems,
+        [filterType]: unTick,
+      });
+    }
+  };
 
   //colors == firebase "colors"
   const productsColor = furnitureData.map((data) => {
@@ -36,34 +101,41 @@ function Filters() {
     return data.name;
   });
 
-  const concatColors = Array.prototype.concat(...productsColor);
+  const concatColors = productsColor.reduce((acc, crr) => acc.concat(crr), []);
+  // new Set automatically delete duplicates
   const colors = [...new Set(concatColors)];
   const categories = [...new Set(productsCategory)];
   const collections = [...new Set(productsCollection)];
 
   const sliderChangeHandler = (e) => {
-    console.log(e.target.value);
-    setSliderValue(e.target.value);
+    if (e.target.name === "min-price") {
+      setSliderValue(parseInt(e.target.value));
+    } else {
+      setSliderMaxValue(parseInt(e.target.value));
+    }
   };
 
   const filterToggle = () => {
     setMobileFilterMenuToggler(!mobileFilterMenuToggler);
   };
 
+  //Best match filter
+  //==>alphabet order
+  //==>price low -> high
+  //==>price high -> low
+
   return (
-    <aside className="filters">
+    <aside className="filters" onChange={changeDetector}>
       <div className="filters__overlay">
         <div onClick={filterToggle} className={`applyField `}>
           <p className="title">Filters</p>
-          <p className={`symbol ${mobileFilterMenuToggler ? "open" : "close"}`}>
-            +
-          </p>
+          <p className={`symbol `}>+</p>
         </div>
       </div>
 
       <div
         className={`filters__underlay ${
-          mobileFilterMenuToggler ? "open" : "close"
+          mobileFilterMenuToggler ? "mobileOpen" : "mobileClose"
         }`}
       >
         <div className="filters__underlay--container">
@@ -80,47 +152,21 @@ function Filters() {
 
           <Filter
             filterType={collections}
-            filterTypeName="Collection"
+            filterTypeName="collection"
             mobileFilterMenuToggler={mobileFilterMenuToggler}
           />
           <Filter
             filterType={colors}
-            filterTypeName="Color"
+            filterTypeName="color"
             mobileFilterMenuToggler={mobileFilterMenuToggler}
           />
           <Filter
             filterType={categories}
-            filterTypeName="Category"
+            filterTypeName="category"
             mobileFilterMenuToggler={mobileFilterMenuToggler}
           />
 
-          <div className="filters__underlay--priceRange">
-            <p className="filters__underlay--priceRange-title">Price Range</p>
-            <div className="filters__underlay--priceRange-sliders">
-              <label htmlFor="min-price">{sliderValue}</label>
-              <input
-                onChange={sliderChangeHandler}
-                className="slider"
-                type="range"
-                name="min-price"
-                min="0"
-                max="500"
-                gap="10"
-                id="min-price"
-              />
-
-              <label htmlFor="max-price">100000</label>
-              <input
-                className="slider"
-                type="range"
-                name="max-price"
-                min="501"
-                max="10,000"
-                gap="10"
-                id="max-price"
-              />
-            </div>
-          </div>
+          <PriceRange />
         </div>
       </div>
     </aside>
