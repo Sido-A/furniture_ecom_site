03f87730519bf39b568073294cc036fb1926b276
Sido-A <sidonie.abe.uk@gmail.com>
2021-03-02 15:49:27 +0000

    delete unused lines

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+
 import BestMatch from "./BestMatch";
 import SearchIcon from "@material-ui/icons/Search";
+import { useStateValue } from "../StateProvider";
 
 function Search() {
+  const [{}, dispatch] = useStateValue();
+  const [searchFiled, setSearchFiled] = useState("");
+  const history = useHistory();
+
+  const preferenceHandler = (e) => {
+    dispatch({
+      type: "PREFERENCE",
+      selectedPreference: e.target.value,
+    });
+  };
+
+  const pushToProductOnFocus = () => {
+    history.push("/products");
+  };
+
+  const searchFieldHandler = (e) => {
+    setSearchFiled(e.target.value);
+  };
+
   return (
-    <div className="sb container container--prl">
+    <div
+      className="sb container container--prl"
+      onChange={pushToProductOnFocus}
+    >
       <div className="search ">
         <div className="search__field">
           <SearchIcon className="searchIcon" />
-          <input type="text" />
+          <input
+            type="text"
+            onChange={searchFieldHandler}
+            value={searchFiled}
+          />
         </div>
       </div>
 
-      <BestMatch />
+      <BestMatch preferenceHandler={preferenceHandler} />
     </div>
   );
 }
