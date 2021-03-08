8337656d98a908d9de350d9dc366891cdc2a46e1
Sido-A <sidonie.abe.uk@gmail.com>
2021-03-08 22:28:12 +0000

    create orders.js

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -4,10 +4,19 @@ import { useStateValue } from "../StateProvider";
 function Orders() {
   const [{ cart }, dispatch] = useStateValue();
   console.log("cart", cart);
+
   return (
-    <div className="orders container">
+    <div className="orders">
       {cart.length !== 0 ? (
-        <h1>Orders</h1>
+        <div className="container">
+          <img className="" src={`${cart[0].image}`} />
+          <div className="">
+            <h3 className="">{cart[0].name}</h3>
+          </div>
+          <div className="">
+            <p className="">Total: £{cart[0].price * cart[0].quantity}</p>
+          </div>
+        </div>
       ) : (
         <div className="orders__empty">
           <h1>Cart is empty</h1>
